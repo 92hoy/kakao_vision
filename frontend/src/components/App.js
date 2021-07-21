3e43db2aeb5e893792ea43ceda333d6532afe5eb
92hoy <ho_9209@naver.com>
2021-07-21 16:52:33 +0900

    Header create

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,34 +1,57 @@
 import React, { Component } from "react";
+// UI
 import { NavLink, Route, Switch } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
 import Home from "./Home";
 import MoimList from "../containers/MoimList";
+import Header from "./Headers";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>MOGAE, 모임 개설</h1>
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+const sections = [
+  { title: "Technology", url: "#" },
+  { title: "Design", url: "#" },
+  { title: "Business", url: "#" },
+  { title: "Health", url: "#" },
+  { title: "Style", url: "#" },
+  { title: "Travel", url: "#" },
+];
+export default function Blog() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <div className="App">
+          <Header title="Blog" sections={sections} />
+          <div className="content-wrapper">
+            <ul>
+              <li>
+                <NavLink exact to="/">
+                  홈으로
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/moim">모임 리스트</NavLink>
+              </li>
+            </ul>
+            <Switch>
+              <Route exact path="/moim" component={MoimList} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
         </div>
-        <div className="content-wrapper">
-          <ul>
-            <li>
-              <NavLink exact to="/">
-                홈으로
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/moim">모임 리스트</NavLink>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/moim" component={MoimList} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
+      </Container>
+    </React.Fragment>
+  );
 }
-
-export default App;
