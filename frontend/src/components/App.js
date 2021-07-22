e03b0ab9a4e61f856efac8400fcaca7d9e7a4002
92hoy <ho_9209@naver.com>
2021-07-22 14:03:18 +0900

    Header Footer Main

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 // UI
-import { NavLink, Route, Switch } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import { NavLink, Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import TwitterIcon from "@material-ui/icons/Twitter";
-
+import Grid from "@material-ui/core/Grid";
 import Home from "./Home";
 import MoimList from "../containers/MoimList";
+import MainFeaturedPost from "./MainFeaturedPost";
+import FeaturedPost from "./FeaturedPost";
 import Header from "./Headers";
+import Footer from "./Footer";
+import post1 from "./blog-post1.md";
+import post2 from "./blog-post2.md";
+import post3 from "./blog-post3.md";
 
-const useStyles = makeStyles((theme) => ({
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
-}));
 const sections = [
   { title: "Technology", url: "#" },
   { title: "Design", url: "#" },
@@ -26,15 +23,53 @@ const sections = [
   { title: "Style", url: "#" },
   { title: "Travel", url: "#" },
 ];
-export default function Blog() {
-  const classes = useStyles();
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random",
+  imgText: "main image description",
+  linkText: "Continue reading…",
+};
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+];
+
+const posts = [post1, post2, post3];
+
+export default function Main() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
         <div className="App">
-          <Header title="Blog" sections={sections} />
-          <div className="content-wrapper">
+          <Header title="Woo Eun Sun" sections={sections} />
+          <main>
+            <MainFeaturedPost post={mainFeaturedPost} />
+            <Grid container spacing={4}>
+              {featuredPosts.map((post) => (
+                <FeaturedPost key={post.title} post={post} />
+              ))}
+            </Grid>
+          </main>
+
+          <div className="content-wrapper" hidden>
             <ul>
               <li>
                 <NavLink exact to="/">
@@ -52,6 +87,11 @@ export default function Blog() {
           </div>
         </div>
       </Container>
+
+      <Footer
+        title="Woo Eun-sun Studio"
+        description=" Photographer, Retoucher"
+      />
     </React.Fragment>
   );
 }
